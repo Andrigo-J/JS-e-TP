52c30fcf6b1c96249b7c73ebca8357c068aecb53
4ndrig0 <andrigo2006ju@gmail.com>
2025-04-05 09:40:24 -0300

    Oloko

---
+++ Aulas/aula14/Untitled-1.js
@@ -0,0 +1,7 @@
+let varA = 'A'; // A
+let varB = 'B'; // B
+let varC = 'C'; // C
+
+[varA, varB, varC] = [varB, varC, varA];
+
+console.log(varA, varB, varC); // B/
---
+++ Aulas/aula14/index.js
@@ -0,0 +1,5 @@
+const batata = document.getElementByClassName('numero-text')
+const mandioca = document.getElementsByClassName('')
+batata.innerHTML = 'batata'
+batata.style.color = 'red'
+batata.style.backgroundColor = 'blue'
