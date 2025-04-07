d13282c10956424f3e14c5fe539a936d3e22cb20
4ndrig0 <andrigo2006ju@gmail.com>
2025-04-07 10:34:42 -0300

    ok

--- Aulas/função/função.js
+++ Aulas/função/função.js
@@ -3,4 +3,16 @@ function saudacao(nome){
 }
 
 const variavel = saudacao('Andrigo');
-console.log(variavel); // undefined, pois a função não retorna nada
+console.log(variavel); // Retorna a string "Olá Andrigo, tudo bem?"
+console.log(saudacao('Andrigo')); // Retorna a string "Olá Andrigo, tudo bem?"
+
+/*
+A função tem a seguinte estrutura:
+function nomeDaFuncao(parametros) {
+    // corpo da função
+    return valor; // opcional
+}
+
+Tem um chamado de função, que é o nome da função seguido de parênteses. Dentro dos parênteses, você pode passar argumentos (valores) para a função. Os parâmetros são variáveis que recebem os valores passados para a função. O corpo da função contém o código que será executado quando a função for chamada. O comando return é usado para retornar um valor da função.
+A função pode retornar um valor, que pode ser armazenado em uma variável ou usado diretamente. Se a função não retornar nada, o valor padrão retornado é undefined.
+*/
